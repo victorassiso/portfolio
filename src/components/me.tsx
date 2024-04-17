c12e0476e53690cc4c05c283e44067e04ea487c6
Victor Assis <victor_assis@poli.ufrj.br>
2024-04-16 23:06:20 -0300

    feat: Add download resume feature

--- src/components/me.tsx
+++ src/components/me.tsx
@@ -1,8 +1,17 @@
+import { Download } from 'lucide-react'
 import Image from 'next/image'
 
 import { cn } from '@/libs/utils'
 
 import { Button } from './ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
 
 interface MeProps {
   className?: string
@@ -29,9 +38,43 @@ export function Me({ className }: MeProps) {
           {/* <button className="tex-xl bg-primary px-6 py-4 font-bold uppercase transition-opacity duration-200 ease-in-out hover:opacity-80">
             DOWNLOAD RESUME
           </button> */}
-          <Button size="lg" className="tex-xl font-bold uppercase">
-            Download Resume
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="lg" className="tex-xl font-bold uppercase">
+                Download Resume
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="!max-w-screen-xs">
+              <DialogHeader>
+                <DialogTitle>Choose a language</DialogTitle>
+                <DialogDescription>
+                  You can download my resume in English or Portuguese.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex justify-around pt-6">
+                <Button asChild>
+                  <a
+                    href="/cv/victor_assis_cv_en.pdf"
+                    download="VICTOR_ASSIS_CV_EN"
+                    className="flex gap-2"
+                  >
+                    <Download />
+                    English
+                  </a>
+                </Button>
+                <Button asChild>
+                  <a
+                    href="/cv/victor_assis_cv_pt.pdf"
+                    download="VICTOR_ASSIS_CV_PT"
+                    className="flex gap-2"
+                  >
+                    <Download />
+                    Português
+                  </a>
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div>
