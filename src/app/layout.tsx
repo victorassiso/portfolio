340a0afa9b2c571a1816356bf75d9aed267fc2c9
Victor Assis <victor_assis@poli.ufrj.br>
2024-04-17 00:05:38 -0300

    feat: New Attempt to set metadata preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Victor Assis | Portfolio',
@@ -15,13 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:title" content="Victor Assis | Portfolio" />
-        <meta property="og:description" content="Description of your page." />
-        <meta property="og:image" content="/thumbnail.png" />
-        <meta property="og:url" content="https://victorassis.vercel.app" />
-        <meta property="og:type" content="website" />
-      </Head>
       <body>{children}</body>
     </html>
   )
